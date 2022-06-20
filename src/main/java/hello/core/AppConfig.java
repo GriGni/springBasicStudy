65369d325c29170ef0ccdabb3f3d2f955b40e202
lcg-howoo <geun@howoocast.com>
2022-06-21 07:54:02 +0900

    AppConfig 릭펙토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,16 +9,28 @@ import hello.core.order.OrderServiceImpl;
 
 /**
  * AppConfig는 객체의 생성과 연결을 담당.
+ * refactoring
+ * repository를 method로 추출해서 가독성을 높임.
+ * 해당 클래스의 역할과 구현 클래스가 한눈에 보인다.
  */
 public class AppConfig {
 
     public MemberService memberService() {
         // 생성자 주입 방식
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+    //repository 역할
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    //interface 기반 구현체.
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    //repository 역할
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
