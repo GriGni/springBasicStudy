c26b3d306068cf99acf0b0b26feafdb8270573f2
lcg-howoo <geun@howoocast.com>
2022-06-21 15:38:03 +0900

    Bean annotation을 사용한 리펙토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,11 +1,15 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * AppConfig는 객체의 생성과 연결을 담당.
@@ -13,24 +17,32 @@ import hello.core.order.OrderServiceImpl;
  * repository를 method로 추출해서 가독성을 높임.
  * 해당 클래스의 역할과 구현 클래스가 한눈에 보인다.
  */
+@Configuration
 public class AppConfig {
-
+    // Bean 에너테이션을 사용하면 스프링 컨테이너에 등록된다.
+    @Bean
     public MemberService memberService() {
         // 생성자 주입 방식
         return new MemberServiceImpl(memberRepository());
     }
+
     //repository 역할
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
     //interface 기반 구현체.
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     //repository 역할
-    private FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+//        할인 정책 변경.
+        return new RateDiscountPolicy();
     }
-
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -5,12 +5,20 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //스프링 컨테이너에 넣어서 관리한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "lee chang geun", Grade.VIP);
         memberService.join(member);
 
