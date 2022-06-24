5cb95a3874b3f30a84a2d98fb552673a1615d5ef
lcg-howoo <geun@howoocast.com>
2022-06-24 10:35:08 +0900

    statless한 서비스 로직 변경

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -1,17 +1,17 @@
 package hello.core.singleton;
 
 public class StatefulService {
+    // 공유되는 값을 지운다.
+//    private int price; // 상태를 유지하는 필드
 
-    private int price; // 상태를 유지하는 필드
-
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name);
         System.out.println("price = " + price);
-
-        this.price = price;
-    }
-
-    public int getPrice () {
         return price;
+//        this.price = price;
     }
+//    public int getPrice () {
+//        return price;
+//    }
+
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,19 +18,21 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA : 사용자A 10000원 주문
-        statefulService1.order("userA", 10000);
+//        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         //ThreadB : 사용자B 20000원 주문
-        statefulService2.order("userb", 20000);
+//        statefulService2.order("userb", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // ThreadA : 사용자A가 주문 금액 조회
-        int price = statefulService1.getPrice();
+//        int price = statefulService1.getPrice();
         // 20000원이 조회 될 것.
         // 이유 : 스프링 컨터이너를 사용하여 싱글톤 패턴이 구현된 상태이므로 서로 상태를 공유하는 상태.
         // statefulService2가 20000원으로 주문함으로써 가져오는 값도 20000원으로 변한다.
-        System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
 
     }
 
