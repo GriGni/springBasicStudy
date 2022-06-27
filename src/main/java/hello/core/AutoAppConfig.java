e292272c11cd1d2393fdcf96682b025dce44429c
lcg-howoo <geun@howoocast.com>
2022-06-28 08:34:28 +0900

    수동 빈 등록시 사용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,7 +1,11 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +20,18 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    @Autowired
+//    MemberRepository memberRepository;
+//    @Autowired
+//    DiscountPolicy discountPolicy;
+//
+//    // 수동으로 bean 등록시시
+//   @Bean
+//    OrderService orderService() {
+//       return new OrderServiceImpl(memberRepository, discountPolicy);
+//    }
+
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
