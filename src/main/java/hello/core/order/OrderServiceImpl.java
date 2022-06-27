3f9744d611bfdfd153bccecc8f8d62d09d727127
lcg-howoo <geun@howoocast.com>
2022-06-28 08:21:51 +0900

    수정자 주입 방식

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,16 +9,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+//    private final MemberRepository memberRepository;
+//    private final DiscountPolicy discountPolicy;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     // 구체에 의존하지 않고 인터페이스에만 의존하게 만들었다.
+    // 생성자로 주입할 경우 args 필수 값이다
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//        this.memberRepository = memberRepository;
+//    }
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    // 스프링 빈에 memberRepository가 등록이 안됐을 수도 있다. 선택적으로 적용 가능 @Autowired(required = false)
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
+    public void setMemberRepository(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
