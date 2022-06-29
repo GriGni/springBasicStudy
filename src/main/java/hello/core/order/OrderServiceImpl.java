8a51d28a0da945e197219e0e35d35d9e1bc9fc1f
lcg-howoo <geun@howoocast.com>
2022-06-30 08:50:43 +0900

    [의존관계 자동 주입] - @Qualifier 사용법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,11 +5,25 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    /*
+     * final 생성되면 바뀌지 않는다. 생성자에서만 값을 넣어줄 수 있다. (나머지는 변경될 수 없다)
+     * */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    //    @RequiredArgsConstructor 가 final로 만든 멤버 필드 생성자를 만들어 준다.
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+        this.memberRepository = memberRepository;
+    }
 
 
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
@@ -29,17 +43,6 @@ public class OrderServiceImpl implements OrderService {
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //        this.discountPolicy = discountPolicy;
 //    }
-    /*
-    * final 생성되면 바뀌지 않는다. 생성자에서만 값을 넣어줄 수 있다. (나머지는 변경될 수 없다)
-    * */
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
-
-//    @RequiredArgsConstructor 가 final로 만든 멤버 필드 생성자를 만들어 준다.
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//        this.memberRepository = memberRepository;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -48,7 +51,6 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
-
 
 
     //테스트 용도
