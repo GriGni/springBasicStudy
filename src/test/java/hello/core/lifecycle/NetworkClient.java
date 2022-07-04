5435c0e4dc6668b716e636a3ecf28ae32965421d
lcg-howoo <geun@howoocast.com>
2022-07-05 08:49:45 +0900

    [빈 생명주기 콜백] - 과거 초기화 방식 (인터페이스 initializingBean, disposableBean)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-        disconnect();
     }
 
     public void setUrl(String url) {
@@ -27,5 +27,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+    // 스프링 빈의 이벤트 라이프 사이클 - 스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 스프링 의존 관계가 끝나면 호출 한다.
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
